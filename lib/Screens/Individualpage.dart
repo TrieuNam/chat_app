2411db23337b922acae183161fef49365d590284
TrieuNam <trieuphuongnam85@gmail.com>
2023-02-25 13:27:45 +0700

    Working on Individual Chat Page

--- lib/Screens/Individualpage.dart
+++ lib/Screens/Individualpage.dart
@@ -1,7 +1,11 @@
+import 'package:chap_app/Model/ChatModel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class IndividualPage extends StatefulWidget {
-  const IndividualPage({Key? key}) : super(key: key);
+  final ChatModel chatModel;
+
+  const IndividualPage({Key? key, required this.chatModel}) : super(key: key);
 
   @override
   State<IndividualPage> createState() => _IndividualPageState();
@@ -10,6 +14,93 @@ class IndividualPage extends StatefulWidget {
 class _IndividualPageState extends State<IndividualPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 70,
+        titleSpacing: 0,
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Row(
+            children: [
+              Icon(
+                Icons.arrow_back,
+                size: 24,
+              ),
+              CircleAvatar(
+                child: SvgPicture.asset(
+                  widget.chatModel.isGroup
+                      ? "assets/" + widget.chatModel.icon
+                      : "assets/" + widget.chatModel.icon,
+                  color: Colors.white,
+                  height: 37,
+                  width: 37,
+                ),
+                radius: 20,
+                backgroundColor: Colors.blueGrey,
+              ),
+            ],
+          ),
+        ),
+        title: InkWell(
+          onTap: (){},
+          child: Container(
+            margin: EdgeInsets.all(6),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.chatModel.name,
+                  style: TextStyle(
+                    fontSize: 18.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "last seen today at 12:05",
+                  style: TextStyle(fontSize: 13),
+                )
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          IconButton( icon: Icon(Icons.videocam),onPressed: (){},),
+          IconButton(onPressed: (){}, icon:Icon(Icons.call)),
+          PopupMenuButton<String>(onSelected: (value) {
+            print(value);
+          }, itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem(
+                child: Text("View Contact"),
+                value: "View contact",
+              ),
+              PopupMenuItem(
+                child: Text("Media links, and docs"),
+                value: "Media links, and docs",
+              ),
+              PopupMenuItem(
+                child: Text("Whatsapp Web"),
+                value: "Whatsapp Web",
+              ),
+              PopupMenuItem(
+                child: Text("Search"),
+                value: "Search",
+              ),
+              PopupMenuItem(
+                child: Text("Mute Notification"),
+                value: "Mute Notification",
+              ),
+              PopupMenuItem(
+                child: Text("wallPaper"),
+                value: "wallPaper",
+              ),
+            ];
+          })
+        ],
+      ),
+    );
   }
 }
