e42b6a50e91f063e7f27f723d1e30ce4f34fbe70
TrieuNam <trieuphuongnam85@gmail.com>
2023-02-25 12:10:58 +0700

    Update source ChatPage, CustomCard , HomeScreen, InvidualPage

--- lib/CustomUI/CustomCard.dart
+++ lib/CustomUI/CustomCard.dart
@@ -1,34 +1,63 @@
+import 'package:chap_app/Model/ChatModel.dart';
+import 'package:chap_app/Screens/Individualpage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({Key? key}) : super(key: key);
+  final ChatModel chatModel;
+
+  const CustomCard({Key? key, required this.chatModel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 25,
-      ),
-      title: Text(
-        "South Million",
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Row(
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => IndividualPage()));
+      },
+      child: Column(
         children: [
-          Icon(Icons.done_all),
-          SizedBox(
-            width: 3,
+          ListTile(
+            leading: CircleAvatar(
+              radius: 25,
+              child: SvgPicture.asset(
+                chatModel.isGroup
+                    ? "assets/" + chatModel.icon
+                    : "assets/" + chatModel.icon,
+                color: Colors.white,
+                height: 37,
+                width: 37,
+              ),
+            ),
+            title: Text(
+              chatModel.name,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(Icons.done_all),
+                SizedBox(
+                  width: 3,
+                ),
+                Text(
+                  chatModel.currentMessage,
+                  style: TextStyle(fontSize: 13),
+                ),
+              ],
+            ),
+            trailing: Text(chatModel.time),
           ),
-          Text(
-            "Hi South Million",
-            style: TextStyle(fontSize: 13),
+          Padding(
+            padding: const EdgeInsets.only(right: 20, left: 80),
+            child: Divider(
+              thickness: 1,
+            ),
           ),
         ],
       ),
-      trailing: Text("18:04"),
     );
   }
 }
--- lib/Pages/ChatPage.dart
+++ lib/Pages/ChatPage.dart
@@ -1,4 +1,5 @@
 import 'package:chap_app/CustomUI/CustomCard.dart';
+import 'package:chap_app/Model/ChatModel.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -9,19 +10,25 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatModel> chats = [
+    ChatModel("South Million", "person.svg", false, "4:00", "Hi EveryOne"),
+    ChatModel("Hanh Duyen", "person.svg", false, "10:00", "Hi South Million"),
+    ChatModel(
+        "Nam Trieu", "groups.svg", true, "3:00", "Hi EveryOne on this group"),
+    ChatModel("Nam Dev", "person.svg", false, "5:00", "Hi EveryOne"),
+    ChatModel("Nam 2", "groups.svg", true, "5:00", "Hi Nam Dev"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.chat),
-      ),
-      body: ListView(
-        children: [
-          CustomCard(),
-          CustomCard(),
-        ],
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.chat),
+        ),
+        body: ListView.builder(
+          itemCount: chats.length,
+          itemBuilder: (context, index) => CustomCard(chatModel: (chats[index])),
+        ));
   }
 }
